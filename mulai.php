fd93413d1672add4871d63a6888de50e52f3c3a3
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-12-20 17:56:29 +0700

    refactor proseskonsul.php mulai.php

--- mulai.php
+++ mulai.php
@@ -1,64 +1,75 @@
 <?php
+
 include "conf/inc.koneksi.php";
+# Baca variabel gejala
+$kdgejala = $_REQUEST['kd_gejala'] ?? "G07";
 
-$NOIP = $_SERVER['REMOTE_ADDR'];
+# Nama gejala berdasarkan kode
+$gejalaList = [
+    "G01" => "sulit mengunyah",
+    "G01a" => "sulit mengunyah",
+    "G02" => "pembengkakan atau peradangan pada gusi",
+    "G02a" => "pembengkakan atau peradangan pada gusi",
+    "G02b" => "pembengkakan atau peradangan pada gusi",
+    "G03" => "gigi bergoyang",
+    "G04" => "rahang terjadi pembengkakan",
+    "G05" => "demam",
+    "G05a" => "demam",
+    "G06" => "pembengkakan kelenjar getah bening sekitar rahang atau leher",
+    "G06a" => "pembengkakan kelenjar getah bening sekitar rahang atau leher",
+    "G07" => "bau mulut tak sedap",
+    "G08" => "rasa sakit atau nyeri di sekitar gusi",
+    "G09" => "rasa sakit yang hebat selama beberapa hari setelah pencabutan gigi",
+    "G10" => "tulang terlihat pada socket",
+    "G11" => "gigi terasa nyilu dan sensitif",
+    "G11a" => "gigi terasa nyilu dan sensitif",
+    "G12" => "bentuk gigi tampak terkikis",
+    "G13" => "sakit kepala",
+    "G14" => "insomnia atau merasa gelisah",
+    "G15" => "suara gemeretak gigi yang terdengar ketika tidur",
+    "G16" => "gusi mudah berdarah",
+    "G16a" => "gusi mudah berdarah",
+    "G17" => "bentuk gusi agak membulat",
+    "G18" => "konsistensi gusi menjadi lunak",
+    "G19" => "gusi atau gigi bernanah",
+    "G19a" => "gusi atau gigi bernanah",
+    "G20" => "gigi terasa sakit atau berdenyut",
+    "G21" => "kemerahan pada sudut-sudut mulut",
+    "G22" => "sudut mulut terasa nyeri",
+    "G23" => "sudut mulut bersisik",
+    "G24" => "ulkus (luka pada sudut mulut)",
+    "G25" => "dentin terlihat",
+    "G26" => "gigi berlubang",
+    "G27" => "pulpa terinfeksi/radang pada pulpa",
+    "G28" => "sakit berdenyut tanpa rangsangan",
+    "G29" => "bintik putih pada gigi",
+    "G30" => "bercak putih pada lidah",
+    "G31" => "bercak putih pada rongga mulut",
+    "G32" => "terdapat endapan plak",
+    "G33" => "terdapat karang gigi",
+    "G34" => "pembusukan gigi",
+    "G35" => "pulpa mati rasa",
+    "G36" => "ruang pulpa terbuka",
+    "G37" => "gusi berwarna merah"
+];
 
-# Periksa apabila sudah ditemukan
-// Periksa data solusi di dalam tmp
-$sql_cekh = "SELECT * FROM tmp_solusi WHERE noip='$NOIP' GROUP BY kd_solusi";
-$qry_cekh = mysqli_query($koneksi, $sql_cekh); // Gunakan mysqli_query
-$hsl_cekh = mysqli_num_rows($qry_cekh);
-if ($hsl_cekh == 1) {
-    // Apabila data tmp_solusi isinya 1
-    $hsl_data = mysqli_fetch_array($qry_cekh);
+# Ambil deskripsi gejala dari array $gejalaList
+$gejalaDeskripsi = $gejalaList[$kdgejala] ?? "Gejala tidak ditemukan";
 
-    // Memindahkan data tmp ke tabel hasil_analisa
-    $sql_pasien = "SELECT * FROM tmp_pasien WHERE noip='$NOIP'";
-    $qry_pasien = mysqli_query($koneksi, $sql_pasien);
-    $hsl_pasien = mysqli_fetch_array($qry_pasien);
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nama'], $_POST['kelamin'], $_POST['alamat'], $_POST['pekerjaan'])) {
+    $nama = mysqli_real_escape_string($koneksi, $_POST['nama']);
+    $kelamin = mysqli_real_escape_string($koneksi, $_POST['kelamin']);
+    $alamat = mysqli_real_escape_string($koneksi, $_POST['alamat']);
+    $pekerjaan = mysqli_real_escape_string($koneksi, $_POST['pekerjaan']);
+    $noip = $_SERVER['REMOTE_ADDR'];
+    $tanggal = date("Y-m-d H:i:s");
 
-    // Perintah untuk memindah data
-    $sql_in = "INSERT INTO analisa_hasil SET
-        nama='{$hsl_pasien['nama']}',
-        kelamin='{$hsl_pasien['kelamin']}',
-        alamat='{$hsl_pasien['alamat']}',
-        pekerjaan='{$hsl_pasien['pekerjaan']}',
-        kd_solusi='{$hsl_data['kd_solusi']}',
-        noip='{$hsl_pasien['noip']}',
-        tanggal='{$hsl_pasien['tanggal']}'";
-    mysqli_query($koneksi, $sql_in);
-
-    // Redireksi setelah pemindahan data
-    echo "<meta http-equiv='refresh' content='0; url=index.php?page=result'>";
-    exit;
-}
-
-# Apabila BELUM MENEMUKAN solusi
-$sqlcek = "SELECT * FROM tmp_analisa WHERE noip='$NOIP'";
-$qrycek = mysqli_query($koneksi, $sqlcek);
-$datacek = mysqli_num_rows($qrycek);
-if ($datacek >= 1) {
-    // Seandainya tmp_analisa tidak kosong
-    $sqlg = "SELECT gejala.* FROM gejala,tmp_analisa
-        WHERE gejala.kd_gejala=tmp_analisa.kd_gejala
-        AND tmp_analisa.noip='$NOIP'
-        AND NOT tmp_analisa.kd_gejala
-        IN(SELECT kd_gejala
-            FROM tmp_gejala WHERE noip='$NOIP')
-        ORDER BY gejala.kd_gejala LIMIT 1";
-    $qryg = mysqli_query($koneksi, $sqlg);
-    $datag = mysqli_fetch_array($qryg);
-
-    $kdgejala = $datag['kd_gejala'];
-    $gejala = $datag['nm_gejala'];
-} else {
-    // Seandainya tmp kosong
-    $sqlg = "SELECT * FROM gejala ORDER BY kd_gejala LIMIT 1";
-    $qryg = mysqli_query($koneksi, $sqlg);
-    $datag = mysqli_fetch_array($qryg);
-
-    $kdgejala = $datag['kd_gejala'];
-    $gejala = $datag['nm_gejala'];
+    // Insert data ke tabel tmp_pasien
+    $sql = "INSERT INTO tmp_pasien (nama, kelamin, alamat, pekerjaan, noip, tanggal) 
+            VALUES ('$nama', '$kelamin', '$alamat', '$pekerjaan', '$noip', '$tanggal')";
+    if (!mysqli_query($koneksi, $sql)) {
+        echo "Error: " . mysqli_error($koneksi);
+    }
 }
 ?>
 
@@ -71,7 +82,7 @@ if ($datacek >= 1) {
             <table class="table" width="100%" border="0" cellpadding="2" cellspacing="1">
                 <tr>
                     <td style="border:none;" colspan="2" align="center">
-                        <h3><span class="label label-default">Apakah <?php echo "$gejala"; ?> ? </span></h3>
+                        <h3><span class="label label-default">Apakah <?php echo $gejalaDeskripsi; ?> ? </span></h3>
                         <input name="TxtKdGejala" type="hidden" value="<?php echo $kdgejala; ?>">
                     </td>
                 </tr>
--- proseskonsul.php
+++ proseskonsul.php
@@ -1,126 +1,129 @@
 <?php
-include "conf/inc.koneksi.php"; // Pastikan koneksi diubah ke mysqli
+include "conf/inc.koneksi.php";
+session_start(); // Memulai session untuk menyimpan data sementara
+$noip = $_SERVER['REMOTE_ADDR'];
 
-# Baca variabel Form (If Register Global ON)
-$RbPilih     = $_REQUEST['RbPilih'];
-$TxtKdGejala = $_REQUEST['TxtKdGejala'];
+# Fungsi untuk menyimpan hasil diagnosa ke tabel analisa_hasil
+function saveDiagnosisResult($kd_solusi, $noip, $koneksi) {
+    // Ambil data pasien dari tabel tmp_pasien berdasarkan IP pengguna
+    $sql_pasien = "SELECT * FROM tmp_pasien WHERE noip = '$noip' ORDER BY id DESC LIMIT 1";
+    $qry_pasien = mysqli_query($koneksi, $sql_pasien) or die("Error: " . mysqli_error($koneksi));
+    $data_pasien = mysqli_fetch_assoc($qry_pasien);
 
-# Mendapatkan No IP
-$NOIP = $_SERVER['REMOTE_ADDR'];
-
-# Fungsi untuk menambah data ke tmp_analisa
-function AddTmpAnalisa($kdgejala, $NOIP, $koneksi) {
-    $sql_solusi = "SELECT rule.* FROM rule 
-                   INNER JOIN tmp_solusi 
-                   ON rule.kd_solusi = tmp_solusi.kd_solusi 
-                   WHERE tmp_solusi.noip = '$NOIP' 
-                   ORDER BY rule.kd_solusi, rule.kd_gejala";
-    $qry_solusi = mysqli_query($koneksi, $sql_solusi);
-    while ($data_solusi = mysqli_fetch_array($qry_solusi)) {
-        $sqltmp = "INSERT INTO tmp_analisa (noip, kd_solusi, kd_gejala)
-                   VALUES ('$NOIP', '{$data_solusi['kd_solusi']}', '{$data_solusi['kd_gejala']}')";
-        mysqli_query($koneksi, $sqltmp);
-    }
-}
-
-# Fungsi menambah data ke tmp_gejala
-function AddTmpGejala($kdgejala, $NOIP, $koneksi) {
-    $sql_gejala = "INSERT INTO tmp_gejala (noip, kd_gejala) VALUES ('$NOIP', '$kdgejala')";
-    mysqli_query($koneksi, $sql_gejala);
-}
-
-# Fungsi menghapus data di tmp_solusi
-function DelTmpSakit($NOIP, $koneksi) {
-    $sql_del = "DELETE FROM tmp_solusi WHERE noip = '$NOIP'";
-    mysqli_query($koneksi, $sql_del);
-}
-
-# Fungsi menghapus data di tmp_analisa
-function DelTmpAnalisa($NOIP, $koneksi) {
-    $sql_del = "DELETE FROM tmp_analisa WHERE noip = '$NOIP'";
-    mysqli_query($koneksi, $sql_del);
-}
-
-# PEMERIKSAAN
-if ($RbPilih == "YA") {
-    $sql_analisa = "SELECT * FROM tmp_analisa WHERE noip = '$NOIP'";
-    $qry_analisa = mysqli_query($koneksi, $sql_analisa);
-    $data_cek = mysqli_num_rows($qry_analisa);
-
-    if ($data_cek >= 1) {
-        # Jika tmp_analisa tidak kosong
-        DelTmpSakit($NOIP, $koneksi);
-
-        $sql_tmp = "SELECT * FROM tmp_analisa 
-                    WHERE kd_gejala = '$TxtKdGejala' 
-                    AND noip = '$NOIP'";
-        $qry_tmp = mysqli_query($koneksi, $sql_tmp);
-
-        while ($data_tmp = mysqli_fetch_array($qry_tmp)) {
-            $sql_rsolusi = "SELECT kd_solusi 
-                            FROM rule 
-                            WHERE kd_solusi = '{$data_tmp['kd_solusi']}' 
-                            GROUP BY kd_solusi";
-            $qry_rsolusi = mysqli_query($koneksi, $sql_rsolusi);
-
-            while ($data_rsolusi = mysqli_fetch_array($qry_rsolusi)) {
-                $sql_input = "INSERT INTO tmp_solusi (noip, kd_solusi)
-                              VALUES ('$NOIP', '{$data_rsolusi['kd_solusi']}')";
-                mysqli_query($koneksi, $sql_input);
-            }
-        }
-
-        DelTmpAnalisa($NOIP, $koneksi);
-        AddTmpAnalisa($TxtKdGejala, $NOIP, $koneksi);
-        AddTmpGejala($TxtKdGejala, $NOIP, $koneksi);
-    } else {
-        # Jika tmp_analisa kosong
-        $sql_rgejala = "SELECT * FROM rule WHERE kd_gejala = '$TxtKdGejala'";
-        $qry_rgejala = mysqli_query($koneksi, $sql_rgejala);
-
-        while ($data_rgejala = mysqli_fetch_array($qry_rgejala)) {
-            $sql_rsolusi = "SELECT kd_solusi 
-                            FROM rule 
-                            WHERE kd_solusi = '{$data_rgejala['kd_solusi']}' 
-                            GROUP BY kd_solusi";
-            $qry_rsolusi = mysqli_query($koneksi, $sql_rsolusi);
-
-            while ($data_rsolusi = mysqli_fetch_array($qry_rsolusi)) {
-                $sql_input = "INSERT INTO tmp_solusi (noip, kd_solusi)
-                              VALUES ('$NOIP', '{$data_rsolusi['kd_solusi']}')";
-                mysqli_query($koneksi, $sql_input);
-            }
-        }
-
-        AddTmpAnalisa($TxtKdGejala, $NOIP, $koneksi);
-        AddTmpGejala($TxtKdGejala, $NOIP, $koneksi);
+    if (!$data_pasien) {
+        die("Data pasien tidak ditemukan. Pastikan data pasien sudah dimasukkan.");
     }
 
-    echo "<meta http-equiv='refresh' content='0; url=index.php?page=start'>";
+    // Data pasien
+    $nama = mysqli_real_escape_string($koneksi, $data_pasien['nama']);
+    $kelamin = mysqli_real_escape_string($koneksi, $data_pasien['kelamin']);
+    $alamat = mysqli_real_escape_string($koneksi, $data_pasien['alamat']);
+    $pekerjaan = mysqli_real_escape_string($koneksi, $data_pasien['pekerjaan']);
+    $tanggal = date("Y-m-d H:i:s");
+
+    // Simpan ke tabel analisa_hasil
+    $sql = "INSERT INTO analisa_hasil (nama, kelamin, alamat, pekerjaan, kd_solusi, noip, tanggal)
+            VALUES ('$nama', '$kelamin', '$alamat', '$pekerjaan', '$kd_solusi', '$noip', '$tanggal')";
+    mysqli_query($koneksi, $sql) or die("Error: " . mysqli_error($koneksi));
 }
 
-if ($RbPilih == "TIDAK") {
-    $sql_analisa = "SELECT * FROM tmp_analisa WHERE noip = '$NOIP'";
-    $qry_analisa = mysqli_query($koneksi, $sql_analisa);
-    $data_cek = mysqli_num_rows($qry_analisa);
+# Baca variabel Form
+$RbPilih = $_REQUEST['RbPilih'] ?? null; // "YA" atau "TIDAK"
+$TxtKdGejala = $_REQUEST['TxtKdGejala'] ?? "G07"; // Gejala awal (default G07)
 
-    if ($data_cek >= 1) {
-        $sql_rule = "SELECT * FROM tmp_analisa WHERE kd_gejala = '$TxtKdGejala'";
-        $qry_rule = mysqli_query($koneksi, $sql_rule);
+# Fungsi untuk menangani diagnosa sesuai pohon keputusan
+function getNextGejalaOrSolusi($currentGejala, $jawaban)
+{
+    // Pohon keputusan berdasarkan diagram
+    $tree = [
+        // G07: Bau mulut
+        "G07" => ["YA" => "G01", "TIDAK" => "G26"],
 
-        while ($hsl_rule = mysqli_fetch_array($qry_rule)) {
-            $sql_deltmp = "DELETE FROM tmp_analisa 
-                           WHERE kd_solusi = '{$hsl_rule['kd_solusi']}' 
-                           AND noip = '$NOIP'";
-            mysqli_query($koneksi, $sql_deltmp);
+        // G01: Sulit mengunyah
+        "G01" => ["YA" => "G04", "TIDAK" => "G16"],
+        "G04" => ["YA" => "G08", "TIDAK" => "P08"],
+        "G08" => ["YA" => "G02", "TIDAK" => "P08"],
+        "G02" => ["YA" => "P08", "TIDAK" => "G05"],
+        "G05" => ["YA" => "G06", "TIDAK" => "P02"],
+        "G06" => ["YA" => "P02", "TIDAK" => "P02"],
 
-            $sql_deltmp2 = "DELETE FROM tmp_solusi 
-                            WHERE kd_solusi = '{$hsl_rule['kd_solusi']}' 
-                            AND noip = '$NOIP'";
-            mysqli_query($koneksi, $sql_deltmp2);
-        }
-    }
+        "G16" => ["YA" => "G02b", "TIDAK" => "G09"],
+        "G09" => ["YA" => "G10", "TIDAK" => "G30"],
+        "G10" => ["YA" => "P03", "TIDAK" => "P03"],
+        "G30" => ["YA" => "G31", "TIDAK" => "P13"], 
+        "G31" => ["YA" => "P13", "TIDAK" => "P13"], 
 
-    echo "<meta http-equiv='refresh' content='0; url=index.php?page=start'>";
+        "G02b" => ["YA" => "G19", "TIDAK" => "G32"],
+        "G19" => ["YA" => "G37", "TIDAK" => "P16"],
+        "G37" => ["YA" => "P16", "TIDAK" => "P16"],
+
+        "G32" => ["YA" => "G33", "TIDAK" => "P14"],
+        "G33" => ["YA" => "P14", "TIDAK" => "P14"],
+
+
+        // G26: Gigi berlubang
+        "G26" => ["YA" => "G25", "TIDAK" => "G02a"], 
+        "G25" => ["YA" => "G11", "TIDAK" => "G29"],
+        "G11" => ["YA" => "P10", "TIDAK" => "G27"],
+        "G27" => ["YA" => "G28", "TIDAK" => "P11"],
+        "G28" => ["YA" => "P11", "TIDAK" => "P11"],
+        "G29" => ["YA" => "P12", "TIDAK" => "P12"], 
+        "G34" => ["YA" => "P35", "TIDAK" => "P15"], 
+        "G35" => ["YA" => "P36", "TIDAK" => "P15"], 
+        "G36" => ["YA" => "P15", "TIDAK" => "P15"], 
+
+        // G26: Jalur tambahan dari G26 -> TIDAK
+        "G02a" => ["YA" => "G01a", "TIDAK" => "G11a"], 
+        "G01a" => ["YA" => "G03", "TIDAK" => "G16a"], 
+        "G03" => ["YA" => "P01", "TIDAK" => "P01"], 
+        "G16a" => ["YA" => "G17", "TIDAK" => "G05a"],
+        "G17" => ["YA" => "G18", "TIDAK" => "P06"],
+        "G18" => ["YA" => "P06", "TIDAK" => "P06"],
+        "G05a" => ["YA" => "G06a", "TIDAK" => "P07"],
+        "G06a" => ["YA" => "G19a", "TIDAK" => "P07"], 
+        "G19a" => ["YA" => "G20", "TIDAK" => "P07"], 
+        "G20" => ["YA" => "P07", "TIDAK" => "P07"],
+
+
+        "G11a" => ["YA" => "G12", "TIDAK" => "G21"],
+        "G12" => ["YA" => "P04", "TIDAK" => "P04"],
+        "G13" => ["YA" => "G14", "TIDAK" => "P05"],
+        "G14" => ["YA" => "G15", "TIDAK" => "P05"],
+        "G15" => ["YA" => "P05", "TIDAK" => "P05"],
+
+        "G21" => ["YA" => "G22", "TIDAK" => "P09"], 
+        "G22" => ["YA" => "G23", "TIDAK" => "P09"], 
+        "G23" => ["YA" => "G24", "TIDAK" => "P09"], 
+        "G24" => ["YA" => "P09", "TIDAK" => "P09"], 
+    ];
+
+    // Kembalikan gejala berikutnya atau solusi
+    return $tree[$currentGejala][$jawaban] ?? "END";
+}
+
+# Proses logika pohon keputusan
+$nextGejala = getNextGejalaOrSolusi($TxtKdGejala, $RbPilih, $noip, $koneksi);
+
+# Simpan jejak gejala ke tmp_analisa
+if ($nextGejala !== "END" && strpos($nextGejala, "P") === false) {
+    $sql = "INSERT INTO tmp_analisa (noip, kd_gejala) VALUES ('$noip', '$TxtKdGejala')";
+    mysqli_query($koneksi, $sql) or die("Error: " . mysqli_error($koneksi));
+}
+
+if (strpos($nextGejala, "P") === 0) {
+    // Simpan hasil diagnosa ke tabel analisa_hasil
+    saveDiagnosisResult($nextGejala, $noip, $koneksi);
+
+    // Redirect ke halaman hasil diagnosa
+    echo "<meta http-equiv='refresh' content='0; url=index.php?page=result'>";
+    exit;
+} elseif ($nextGejala == "END") {
+    // Jika tidak ditemukan solusi
+    echo "<meta http-equiv='refresh' content='0; url=index.php?page=result&status=tidak_ditemukan'>";
+    exit;
+} else {
+    // Redirect ke gejala berikutnya
+    echo "<meta http-equiv='refresh' content='0; url=index.php?page=start&kd_gejala=$nextGejala'>";
+    exit;
 }
 ?>
